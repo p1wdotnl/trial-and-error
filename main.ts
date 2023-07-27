f3bb578315d72573bf159088c493d73b1f6d0262
Elvis has left the building <developer@spooky.demon.nl>
2023-07-27 15:52:36 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -10,9 +10,20 @@ input.onButtonPressed(Button.A, function () {
     motorbit.forward(60)
     basic.pause(500)
     motorbit.turnright(50)
-    basic.pause(1000)
+    basic.pause(3000)
     motorbit.turnleft(50)
+    basic.pause(3000)
+    motorbit.forward(20)
+    basic.pause(2000)
     basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
 })
+// v0.0..0
 basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P4) == 0) {
+        basic.showNumber(0)
+    } else {
+        basic.showNumber(1)
+    }
+    basic.pause(500)
+})
